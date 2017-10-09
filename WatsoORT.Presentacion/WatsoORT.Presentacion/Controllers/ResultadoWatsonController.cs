a6b5ae7b7dcd9717877432c14b2a8ea7ccabc6e7
Gonzalo <gonzajb@gmail.com>
2017-10-08 23:10:27 -0300

    Controlador de resultado

---
+++ WatsoORT.Presentacion/WatsoORT.Presentacion/Controllers/ResultadoWatsonController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WatsonORT.Presentacion.Models;
+using WatsonORT.Datos.Repositorios;
+using WatsonORT.Dominio.Clases;
+
+namespace WatsonORT.Presentacion.Controllers
+{
+    public class ResultadoWatsonController : Controller
+    {
+        private ConsultaAnalisisRepository consultaAnalisis = new ConsultaAnalisisRepository();
+        // GET: ResultadoWatson
+        public ActionResult IngresoCodigo()
+        {
+            return View();
+        }
+        // POST: ResultadoWatson
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult IngresoCodigo(IngresoCodigoViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var consulta = new ConsultaAnalisis();
+                consulta = consultaAnalisis.GetConsultaByCodigo(model.CodigoConsulta);
+                if (consulta == null)
+                {
+                    ModelState.AddModelError("Codigo de consulta incorrecto", "Codigo de consulta incorrecto");
+                    return View(model);
+                }               
+;                
+                return RedirectToAction("Resultado", "ResultadoWatson");
+
+            }
+            return View(model);
+        }
+        public ActionResult Resultado()
+        {
+            return View();
+        }
+    }
+}
---
+++ WatsoORT.Presentacion/WatsoORT.Presentacion/Models/IngresoCodigoViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WatsonORT.Presentacion.Models
+{
+    public class IngresoCodigoViewModel
+    {
+        [Required]
+        [Display(Name = "Codigo")]
+        public string CodigoConsulta { get; set; }
+    }
+}
