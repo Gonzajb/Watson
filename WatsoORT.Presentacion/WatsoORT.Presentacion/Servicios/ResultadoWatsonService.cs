a2ae1a167023afee12854b3934d8a3579ec1d092
langStrife <langstrife@gmail.com>
2017-10-13 19:44:15 -0300

    ResultadoWatsonService.cs agregado

---
+++ WatsoORT.Presentacion/WatsoORT.Presentacion/Servicios/ResultadoWatsonService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace WatsonORT.Presentacion.Servicios
+{
+    public class ResultadoWatsonService
+    {
+ 
+        string uri = "http://jony-personality.mybluemix.net/api/personality";
+
+        public async Task<String> PostAsync(string uri, string data)
+        {
+            var httpClient = new HttpClient();
+            var response = await httpClient.PostAsync(uri, new StringContent(data));
+
+            response.EnsureSuccessStatusCode();
+            string content = await response.Content.ReadAsStringAsync();
+            return content;
+        }
+    }
+}
